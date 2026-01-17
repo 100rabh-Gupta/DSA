108c7a0d8bd55de2342e7b91eda769f4ea31ee46
100rabh-Gupta <100rabhgupta8400@gmail.com>
2026-01-17 22:29:07 +0530

    Adding product file

---
+++ arrproduct.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int prod(int arr[],int ind){
+    
+    if ( ind==0){
+        return arr[0];
+    }
+
+    return arr[ind]* prod( arr,ind-1);
+}
+void Print( int n,int arr[],int index){
+    if (index==n) return ;
+
+    cout<<arr[index]<<" ";
+    Print( n,arr,index+1);
+}
+
+int main(){
+    int inde=0;
+    int arr[]={2,4,1,6,5};
+    int n=sizeof(arr)/ sizeof(arr[0]);
+    cout<<"Array : ";
+     Print(n,arr,0);
+
+     cout<<endl<<prod(arr,n-1);}
